b6cce07529efcdd939f6426e64eb589bd0c033bd
jkaunert <jkaunert@clusterfly.net>
2018-11-16 11:23:32 -0600

    Adds app skeleton layout and controllers

---
+++ ColorList/ColorList/DetailViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+    @IBOutlet var likeButtons: [UIButton]!
+    @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var swatchView: UIView!
+    @IBOutlet weak var colorView: UIView!
+}
---
+++ ColorList/ColorList/TableViewController.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class TableViewController: UITableViewController {
+    let reuseIdentifier = "crayon overview"
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return CrayonHelper.shared.sectionCount
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return CrayonHelper.shared.rowCountFor(section: section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.reuseIdentifier, for: indexPath) as? TableViewCell else {fatalError("unable to decue cell")}
+        cell.colorLabel.text = CrayonHelper.shared.crayonFor(indexPath: indexPath).name
+        //cell.colorView.image = CrayonHelper.shared.crayonFor(indexPath: indexPath).image
+        
+        return cell
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.backgroundColor = .lightGray
+        label.text = CrayonHelper.shared.sectionNameFor(indexPath: IndexPath(row: 0, section: section))
+        return label
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return CrayonHelper.shared.sectionTitles()
+    }
+    
+}
