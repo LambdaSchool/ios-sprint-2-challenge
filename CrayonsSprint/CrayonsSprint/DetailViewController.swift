0f3e2dd14fb20acfc5937f5e51f3ebdabcc23618
Frulwinn <frulwinn@gmail.com>
2018-11-16 10:17:50 -0800

    setup detailview and made outlet connections

--- CrayonsSprint/CrayonsSprint/DetailViewController.swift
+++ CrayonsSprint/CrayonsSprint/DetailViewController.swift
@@ -2,5 +2,17 @@ import UIKit
 
 class DetailViewController: UITableViewController {
     
+    @IBOutlet weak var crayonView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var swatchView: UIView!
+    
+    @IBOutlet weak var specLabel: UILabel!
+    
+    @IBAction func likeButton(_ sender: Any) {
+        
+        
+    }
     
 }
