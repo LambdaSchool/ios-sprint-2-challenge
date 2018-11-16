91439e0e1cfd5fec5aa410d6682d35264b51587c
Ben Hakes <bhhakes@gmail.com>
2018-11-16 10:29:24 -0600

    add constraints to TVC, set reuseID, add IB outlets to TVC.sw

--- Crayon/Crayon/TableViewCell.swift
+++ Crayon/Crayon/TableViewCell.swift
@@ -2,6 +2,11 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
+    static let resuseIdentifier = "cell"
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var leftViewBuffer: UIView!
+    @IBOutlet weak var rightViewBuffer: UIView!
+    @IBOutlet weak var swatchView: UIImageView!
     
 }
