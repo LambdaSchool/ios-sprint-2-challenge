f5d8d7b1c18173ba472e044ad24fa61ce0103eac
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-16 09:17:32 -0700

    Initial storyboard setup complete.

---
+++ ios-sprint-2-challenge/ios-sprint-2-challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-sprint-2-challenge
+//
+//  Created by Ariel M. McLaughlin on 11/16/18.
+//  Copyright © 2018 Ariel M. McLaughlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
