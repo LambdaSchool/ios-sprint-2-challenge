64969efbd8dc0a63a35ed4d9315dee1404d299aa
Frulwinn <frulwinn@gmail.com>
2018-11-16 08:39:28 -0800

    set up objects in IB and created outlets

--- CrayonsSprint/CrayonsSprint/TableViewController.swift
+++ CrayonsSprint/CrayonsSprint/TableViewController.swift
@@ -2,6 +2,22 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
-  
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        <#code#>
+    }
+    
+    //will need prepare for segue
 }
 
