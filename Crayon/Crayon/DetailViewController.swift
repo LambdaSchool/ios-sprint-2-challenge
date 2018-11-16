624db2b2158102300a86e895e39079ca4a9d822f
Ben Hakes <bhhakes@gmail.com>
2018-11-16 11:50:37 -0600

    fix color issues of DVC by embedding stack view in view

--- Crayon/Crayon/DetailViewController.swift
+++ Crayon/Crayon/DetailViewController.swift
@@ -10,29 +10,37 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var crayonLiteralLabel: UILabel!
     @IBOutlet weak var topBufferView: UIView!
     @IBOutlet weak var bottomBufferView: UIView!
-    @IBOutlet var detailView: UIView!
     @IBOutlet weak var topBufferLabel: UILabel!
     @IBOutlet weak var bottomBufferLabel: UILabel!
-    
+    @IBOutlet weak var inlaidView: UIView!
+    @IBOutlet var detailView: UIView!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         guard let crayon = crayon else {return}
         
+        inlaidView.backgroundColor = .white
         detailView.backgroundColor = crayon.color
         crayonImageView.image = crayon.image
         crayonNameLabel.text = crayon.name
         crayonColorView.backgroundColor = crayon.color
         topBufferView.backgroundColor = crayon.color
         bottomBufferView.backgroundColor = crayon.color
-        bottomBufferLabel.text = ":-)"
-        topBufferLabel.text = ":-)"
         crayonLiteralLabel.text = crayon.color.description
         
+        if(crayon.isLiked) {
+            bottomBufferLabel.text = "❤️"
+            topBufferLabel.text = "❤️"
+        } else {
+            bottomBufferLabel.text = "🤔"
+            topBufferLabel.text = "🤔"
+        }
+        
     }
-    /*
-    @IBAction func save(_ sender: Any) {
+    
+    @IBAction func bottom(_ sender: Any) {
+        
         guard let person = person else {return}
         guard let name = nameField.text, !name.isEmpty else {return}
         person.name = name
@@ -44,7 +52,12 @@ class DetailViewController: UIViewController {
         
         
     }
-    */
+    
+    func toggleLiked {
+        
+    }
+    
+    
     
     
 }
