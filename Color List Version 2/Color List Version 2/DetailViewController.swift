5509b59bf1756eeeea53119519b59148bb944671
IvanCaldwell <grumpy.caldwell@gmail.com>
2018-11-17 10:27:47 -0800

    Basic Setup

---
+++ Color List Version 2/Color List Version 2/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  Color List Version 2
+//
+//  Created by Ivan Caldwell on 11/17/18.
+//  Copyright © 2018 Ivan Caldwell. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
---
+++ Color List Version 2/Color List Version 2/TableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewController.swift
+//  Color List Version 2
+//
+//  Created by Ivan Caldwell on 11/17/18.
+//  Copyright © 2018 Ivan Caldwell. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableVieWController: UITableViewController {
+    
+}
