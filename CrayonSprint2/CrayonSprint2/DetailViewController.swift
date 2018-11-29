163a772accd680b97af6735b9b22c0f506094c3b
Frulwinn <frulwinn@gmail.com>
2018-11-29 10:33:07 -0800

    fixed emoji button had to get rid of view behind button

--- CrayonSprint2/CrayonSprint2/DetailViewController.swift
+++ CrayonSprint2/CrayonSprint2/DetailViewController.swift
@@ -5,13 +5,11 @@ class DetailViewController: UIViewController {
     var crayon: Crayon?
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var whiteView: UIView!
-    @IBOutlet weak var bottomBarView: UIView!
     @IBOutlet weak var likeButton2: UIButton!
     @IBOutlet weak var crayonView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var swatchView: UIView!
     @IBOutlet weak var detailLabel: UILabel!
-    @IBOutlet weak var topBarView: UIView!
     @IBOutlet weak var likeButton1: UIButton!
     
     override func viewWillAppear(_ animated: Bool) {
@@ -20,18 +18,29 @@ class DetailViewController: UIViewController {
         guard let crayon = crayon else {return}
         backgroundView.backgroundColor = crayon.color
         whiteView.backgroundColor = .white
-        bottomBarView.backgroundColor = crayon.color
-        topBarView.backgroundColor = crayon.color
         crayonView.image = crayon.image
         nameLabel.text = crayon.name
         swatchView.backgroundColor = crayon.color
         
     }
-
+    //action button
     @IBAction func didPressLike(_ sender: Any) {
         guard let crayon = crayon else {return}
         CrayonHelper.shared.tappedLikeButton(for: crayon)
+        updateViews()
     }
     
-    
+    //defining what happens when i click what the button should do 
+    func updateViews() {
+        guard let crayon = crayon else {return}
+        
+        var newButtonTitle: String
+        if crayon.isLiked {
+            newButtonTitle = "😻"
+        } else {
+            newButtonTitle = "😾"
+        }
+        likeButton1.setTitle(newButtonTitle, for: .normal)
+        likeButton2.setTitle(newButtonTitle, for: .normal)
+    }
 }
