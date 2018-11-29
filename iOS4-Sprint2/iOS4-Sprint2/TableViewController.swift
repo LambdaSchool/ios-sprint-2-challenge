9517ecf85c436d1d1813bf68ee1c70a9237693b3
Vijay Das <vijaytdas@gmail.com>
2018-11-29 13:02:31 -0600

    TVC completed. WIPL DVC and autolayout fixes

--- iOS4-Sprint2/iOS4-Sprint2/TableViewController.swift
+++ iOS4-Sprint2/iOS4-Sprint2/TableViewController.swift
@@ -8,39 +8,55 @@ class TableViewController: UITableViewController {
         return CrayonHelper.shared.sectionCount
     }
     
-    // section header
+    // section header text and display attributes
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        <#code#>
+        let label = UILabel()
+        label.textAlignment = .center
+        label.backgroundColor = .black
+        label.textColor = .white
+        label.text = CrayonHelper.shared.sectionNameFor(indexPath: IndexPath(row: 0, section: section))
+        return label
     }
     
     // number of rows
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return CrayonHelper.shared.rowCountFor(section: section)
+        
     }
     
     // cellForRowAt
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.reuseIdentifier, for: indexPath) as? TableViewCell else { fatalError("Could not dequeue cell")}
    
-        
         // code to setup the tableview cell
-        //
+        
+        let crayon = CrayonHelper.shared.crayonFor(indexPath: indexPath)
+        
+        cell.cellBorder.backgroundColor = crayon.color
+        cell.nameLabel.text = crayon.name
+        cell.crayonView.image = crayon.image
         
         
         
+ return cell
     }
-    
     
     // prepare for seque
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // identify the cell to go to detail view
         
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        guard let destination = segue.destination as? DetailViewController else { return }
         
+        let crayon = CrayonHelper.shared.crayonFor(indexPath: indexPath)
         
+        destination.crayon = crayon
         
+               
     }
     
     
