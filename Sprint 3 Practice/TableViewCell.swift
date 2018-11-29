42d95f523222909676b6ae313ee753723b66eb6b
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-11-29 09:27:48 -0800

    connect table view cell to storyboard
    and add two buttons to the DVC

--- Sprint 3 Practice/TableViewCell.swift
+++ Sprint 3 Practice/TableViewCell.swift
@@ -1,5 +1,10 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
+    static let reusableIdentifier = "cell"
+    @IBOutlet weak var crayonNameMain: UILabel!
+    @IBOutlet weak var crayonImageMain: UIImageView!
+    @IBOutlet weak var leadingSwatch: UIView!
+    @IBOutlet weak var trailingSwatch: UIView!
     
 }
