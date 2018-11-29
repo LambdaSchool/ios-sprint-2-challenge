bcb455233c0539b0e7bd7d93452992d4429cb451
Frulwinn <frulwinn@gmail.com>
2018-11-29 09:03:16 -0800

    hooked up objects and made outlets from IB to tableviewcell

--- CrayonSprint2/CrayonSprint2/TableViewCell.swift
+++ CrayonSprint2/CrayonSprint2/TableViewCell.swift
@@ -2,6 +2,14 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
-   
+   static let reuseIdentifier = "crayonCell"
 
+    @IBOutlet weak var leftView: UIView!
+    
+    @IBOutlet weak var rightView: UIView!
+    
+    @IBOutlet weak var crayonView: UIImageView!
+    
+    @IBOutlet weak var colorLabel: UILabel!
+    
 }
