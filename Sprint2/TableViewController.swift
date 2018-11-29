b95a8b6b96c67081798e75cda1cd1ba950054eba
Sean Doyle <sdoylelambda@gmail.com>
2018-11-29 12:06:54 -0500

    1 hour in

--- Sprint2/TableViewController.swift
+++ Sprint2/TableViewController.swift
@@ -24,59 +24,22 @@ class TableViewController: UITableViewController {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? TableViewCell
             else { fatalError("U killed puppies") }
         
-        cell.nameLabel.text = CrayonHelper.shared.crayonFor(indexPath: indexPath)
-        cell.swatchView.backgroundColor = CrayonHelper.shared.crayonFor(indexPath: indexPath)
+        cell.nameLabel.text = CrayonHelper.shared.sectionNameFor(indexPath: indexPath)
+ //       cell.swatchView.backgroundColor = CrayonHelper.shared.crayonFor(indexPath: indexPath)
         return cell
     }
     
 
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.backgroundColor = .black
+        label.textColor = .white
+        label.text = CrayonHelper.shared.sectionNameFor(indexPath: IndexPath(row: 0, section: section))
+        return label
+    }
     
-    // dont think i need
-    
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return CrayonHelper.shared.sectionTitles()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
