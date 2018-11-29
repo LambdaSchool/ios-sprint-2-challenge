4d68b07d7da36a98323c048ad951598e35a7db14
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2018-11-29 12:37:41 -0500

    add function to display items when view appears to user

--- CrayonList/CrayonList/DetailViewController.swift
+++ CrayonList/CrayonList/DetailViewController.swift
@@ -1,26 +1,31 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    var crayon: Crayon?
     
     @IBOutlet var topButton: UIButton!
     @IBOutlet var bottomButton: UIButton!
     @IBOutlet var topSwatchView: UIView!
     @IBOutlet var bottomSwatchView: UIView!
-    @IBOutlet var detailView: UIView!
+    @IBOutlet var detailView: UIImageView!
     @IBOutlet var detailLabel: UILabel!
     @IBOutlet var detailSwatch: UIView!
     @IBOutlet var backgroundView: UIView!
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        guard let crayon = crayon else {
-            return
-        }
+        guard let crayon = crayon else { return}
+        topSwatchView.backgroundColor = crayon.color
+        bottomSwatchView.backgroundColor = crayon.color
+        detailSwatch.backgroundColor = crayon.color
+        backgroundView.backgroundColor = crayon.color
+        detailView.image = crayon.image
+        detailLabel.text = crayon.name
         
-        
+    
+
+
     }
 
 
 }
-
-
