e22df995c131a8ff476baa4d9a7df79eb0dda78a
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-29 09:40:27 -0700

    Added outlets and actions for the DetailViewController

--- ios-sprint2-challenge/ios-sprint2-challenge/DetailViewController.swift
+++ ios-sprint2-challenge/ios-sprint2-challenge/DetailViewController.swift
@@ -5,9 +5,26 @@ class DetailViewController: UIViewController {
     
     var crayon: Crayon?
     
+    @IBOutlet weak var likeButton1: UIButton!
+    
+    @IBOutlet weak var likeButton2: UIButton!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var crayonImage: UIImageView!
+    
+    @IBOutlet weak var swatchView: UIView!
+    
+    @IBAction func tapButton1(_ sender: Any) {
+    }
+    
+    @IBAction func tapButton2(_ sender: Any) {
+    }
+    
+
     
     
-    
+
     
     
     
