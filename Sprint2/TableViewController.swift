61146022351e82f2707425bcc1a937f9e1beff3f
Sean Doyle <sdoylelambda@gmail.com>
2018-11-29 13:24:30 -0500

    Table shows headers and names, back button implemented

--- Sprint2/TableViewController.swift
+++ Sprint2/TableViewController.swift
@@ -1,47 +1,42 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.tintColor = .red
     }
     
-    @IBOutlet weak var crayonView: UIImageView!
-
-    var crayons: [Crayon] = []
-
-    // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return CrayonHelper.shared.sectionCount
+        return ColorHelper.shared.sectionCount
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return CrayonHelper.shared.rowCountFor(section: section)
+        return ColorHelper.shared.rowCountFor(section: section)
     }
-
-    let reuseIdentifier = "cell"
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? TableViewCell
-            else { fatalError("U killed puppies") }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.reuseIdentifier, for: indexPath) as? TableViewCell
+            else { fatalError("Unable to dequeue proper cell") }
         
-        cell.nameLabel.text = CrayonHelper.shared.colorNameFor(indexPath: indexPath)
-        cell.swatchView.backgroundColor = CrayonHelper.shared.colorFor(indexPath: indexPath)
+   
+        cell.nameLabel.text = ColorHelper.shared.colorNameFor(indexPath: indexPath)
+        cell.swatchView.backgroundColor = ColorHelper.shared.colorFor(indexPath: indexPath)
+        
         return cell
     }
     
-
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+
         let label = UILabel()
         label.textAlignment = .center
         label.backgroundColor = .black
         label.textColor = .white
-        label.text = CrayonHelper.shared.sectionNameFor(indexPath: IndexPath(row: 0, section: section))
+        label.text = ColorHelper.shared.sectionNameFor(indexPath: IndexPath(row: 0, section: section))
         return label
     }
     
     override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return CrayonHelper.shared.sectionTitles()
+        return ColorHelper.shared.sectionTitles()
     }
 }
--- Sprint2/ViewController.swift
+++ Sprint2/ViewController.swift
@@ -1,7 +1,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-
+    
+    @IBOutlet weak var showView: UIView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    
+    
+ //   guard let showView = Cra
+    }
 }
 
