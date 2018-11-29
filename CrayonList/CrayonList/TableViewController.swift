e3d3f162e1849b662c58aca155a4f4b937b9d6b4
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2018-11-29 13:11:57 -0500

    update tvc file

--- CrayonList/CrayonList/TableViewController.swift
+++ CrayonList/CrayonList/TableViewController.swift
@@ -4,15 +4,29 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return CrayonHelper.shared.sectionCount
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return CrayonHelper.shared.rowCountFor(section: section)
+    }
     
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.reuseIdentifier, for: indexPath) as? TableViewCell else { fatalError("Unable to dequeue cell") }
+        
+        cell.crayonLabel.text = CrayonHelper.shared.crayonFor(indexPath: indexPath).name
+        cell.crayonView.image = CrayonHelper.shared.crayonFor(indexPath: indexPath).image
+        return cell
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    
 
 }
 
