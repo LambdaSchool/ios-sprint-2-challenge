ea998a59968c6b91cdf943193c6a5e424f34415d
Frulwinn <frulwinn@gmail.com>
2018-11-29 09:14:06 -0800

    added tableview functions numberOfSections, numberOfRows, cellForRow

--- CrayonSprint2/CrayonSprint2/TableViewController.swift
+++ CrayonSprint2/CrayonSprint2/TableViewController.swift
@@ -1,6 +1,28 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-
+    
+//tableview functions
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return CrayonHelper.shared.sectionCount
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return CrayonHelper.shared.rowCountFor(section: section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.reuseIdentifier, for: indexPath) as? TableViewCell else { fatalError("unable to dequeue cell")}
+        
+        let crayon = CrayonHelper.shared.crayonFor(indexPath: indexPath)
+        
+        cell.colorLabel.text = crayon.name
+        cell.crayonView.image = crayon.image
+        cell.leftView.backgroundColor = crayon.color
+        cell.rightView.backgroundColor = crayon.color
+        
+        return cell
+        
+    }
 }
 
