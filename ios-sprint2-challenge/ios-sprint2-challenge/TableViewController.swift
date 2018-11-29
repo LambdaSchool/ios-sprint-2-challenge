c000c5b634b52079c5a1fa2a35b99dbaade83ded
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-29 09:22:56 -0700

    Functions added to the TableViewController for basic functionality.

--- ios-sprint2-challenge/ios-sprint2-challenge/TableViewController.swift
+++ ios-sprint2-challenge/ios-sprint2-challenge/TableViewController.swift
@@ -3,4 +3,18 @@ import Foundation
 
 class TableViewController: UITableViewController {
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return CrayonHelper.shared.sectionCount
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return CrayonHelper.shared.rowCountFor(section: section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.reuseIdentifier, for: indexPath) as? TableViewCell else { fatalError("Unable to dequeue cell") }
+        
+        //Need to come back as soon as I connect my outlets
+        return cell
+    }
 }
