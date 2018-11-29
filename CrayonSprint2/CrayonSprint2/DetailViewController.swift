db77c41ac79c6e0e6f73507b7edc7c497d36d57b
Frulwinn <frulwinn@gmail.com>
2018-11-29 10:02:47 -0800

    added outlets to objects for detailviewcontroller

--- CrayonSprint2/CrayonSprint2/DetailViewController.swift
+++ CrayonSprint2/CrayonSprint2/DetailViewController.swift
@@ -3,6 +3,16 @@ import UIKit
 class DetailViewController: UIViewController {
     
     var crayon: Crayon?
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var whiteView: UIView!
+    @IBOutlet weak var bottomBarView: UIView!
+    @IBOutlet weak var likeButton2: UIButton!
+    @IBOutlet weak var crayonView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var swatchView: UIView!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var topBarView: UIView!
+    @IBOutlet weak var likeButton1: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
