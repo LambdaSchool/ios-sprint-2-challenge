3f714f0f68434f229626816baa48fb5c3bf5ffff
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-11-29 09:51:44 -0800

    add section headers

--- Sprint 3 Practice/TableViewController.swift
+++ Sprint 3 Practice/TableViewController.swift
@@ -2,4 +2,18 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
+    // MVP - You will include section headers grouped by the first letter of each crayon.
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.backgroundColor = .black
+        label.textColor = .white
+        label.text = CrayonHelper.shared.sectionNameFor(indexPath: IndexPath(row: 0, section: section))
+        return label
+    }
+    
+    
+    
+    
 }
