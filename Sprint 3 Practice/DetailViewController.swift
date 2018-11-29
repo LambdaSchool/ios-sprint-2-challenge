9f58743556895936bbb8e65d59eb52d09a65093c
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-11-29 09:36:06 -0800

    connect detail storyboard UI to detail view controller

--- Sprint 3 Practice/DetailViewController.swift
+++ Sprint 3 Practice/DetailViewController.swift
@@ -1,5 +1,25 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    @IBOutlet weak var largestBackground: UIView!
     
+    @IBOutlet weak var secondLargestView: UIView!
+    
+    @IBOutlet weak var topSwatch: UIView!
+    
+    @IBOutlet weak var bottomSwatch: UIView!
+    
+    @IBOutlet weak var crayonDetailImageView: UIImageView!
+    
+    @IBOutlet weak var crayonNameDetail: UILabel!
+    
+    @IBOutlet weak var crayonSwatch: UIView!
+    
+    @IBOutlet weak var crayonColors: UILabel!
+    
+    @IBAction func likeButtonTop(_ sender: Any) {
+    }
+    
+    @IBAction func likeButtonBottom(_ sender: Any) {
+    }
 }
