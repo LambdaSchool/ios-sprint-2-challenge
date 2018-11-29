b1136094f91b8ecb68aa6d0d4a6fa32094695d5d
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-11-29 10:53:09 -0800

    added color and text data to the detail view controller objects/elements
    Forgot to push this sooner.

--- Sprint 3 Practice/DetailViewController.swift
+++ Sprint 3 Practice/DetailViewController.swift
@@ -15,6 +15,7 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var crayonSwatch: UIView!
     
+    // Omitted from final project, currently set as hiden. Name is Crayon Colors.
     @IBOutlet weak var crayonColors: UILabel!
     
     @IBAction func likeButtonTop(_ sender: Any) {
@@ -25,9 +26,26 @@ class DetailViewController: UIViewController {
     
     var crayon: Crayon?
 
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         guard let crayon = crayon else { fatalError("No crayon found.")}
+        
+        
+    // The detail view includes the crayon image, its name, and a swatch on a white background. Behind the white background, the main view is tinted with the crayon color.
+        
+        // Crayon Colors (is there a way to condense this?)
+        largestBackground.backgroundColor = crayon.color
+        topSwatch.backgroundColor = crayon.color
+        crayonSwatch.backgroundColor = crayon.color
+        bottomSwatch.backgroundColor = crayon.color
+        // crayonColors.text = "..."
+        
+        // Everything else
+        secondLargestView.backgroundColor = .white
+        crayonDetailImageView.image = crayon.image
+        crayonNameDetail.text = crayon.name
+        
     }
 }
