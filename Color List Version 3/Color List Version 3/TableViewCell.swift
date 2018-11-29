63570d19fbc6c88f63b40c0922567eda15369190
IvanCaldwell <grumpy.caldwell@gmail.com>
2018-11-29 08:40:53 -0800

    File Setup

---
+++ Color List Version 3/Color List Version 3/TableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewCell.swift
+//  Color List Version 3
+//
+//  Created by Ivan Caldwell on 11/29/18.
+//  Copyright © 2018 Ivan Caldwell. All rights reserved.
+//
+
+import Foundation
