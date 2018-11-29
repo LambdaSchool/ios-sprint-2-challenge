4ca05f24826821a61143dbab3a1d0a8c26165f15
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2018-11-29 12:25:08 -0500

    connect outlets in detail vc to IB

--- CrayonList/CrayonList/DetailViewController.swift
+++ CrayonList/CrayonList/DetailViewController.swift
@@ -2,4 +2,25 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet var topButton: UIButton!
+    @IBOutlet var bottomButton: UIButton!
+    @IBOutlet var topSwatchView: UIView!
+    @IBOutlet var bottomSwatchView: UIView!
+    @IBOutlet var detailView: UIView!
+    @IBOutlet var detailLabel: UILabel!
+    @IBOutlet var detailSwatch: UIView!
+    @IBOutlet var backgroundView: UIView!
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let crayon = crayon else {
+            return
+        }
+        
+        
+    }
+
+
 }
+
+
