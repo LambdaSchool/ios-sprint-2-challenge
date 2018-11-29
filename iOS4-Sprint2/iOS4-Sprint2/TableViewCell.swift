4c80d55f48551b9cb52b1467939881ec2dc375e4
Vijay Das <vijaytdas@gmail.com>
2018-11-29 11:04:03 -0600

    add crayon image assets, table view cell contents and outlets

--- iOS4-Sprint2/iOS4-Sprint2/TableViewCell.swift
+++ iOS4-Sprint2/iOS4-Sprint2/TableViewCell.swift
@@ -5,6 +5,7 @@ class TableViewCell: UITableViewCell {
     static let reuseIdentifier = "cell"
     // need nameLabel and swatchView outlets
     
+    @IBOutlet weak var nameLabel: UILabel!
     
     
 }
