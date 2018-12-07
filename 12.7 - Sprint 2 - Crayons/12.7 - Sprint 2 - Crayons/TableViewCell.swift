f593b2b715128a81b144007dd0c797020ad88c77
Sean Doyle <sdoylelambda@gmail.com>
2018-12-07 12:10:03 -0500

    Model, Table VC updated

--- 12.7 - Sprint 2 - Crayons/12.7 - Sprint 2 - Crayons/TableViewCell.swift
+++ 12.7 - Sprint 2 - Crayons/12.7 - Sprint 2 - Crayons/TableViewCell.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
+    
+    static let reuseIdentifier = "cell"
 
     @IBOutlet weak var swatchView01: UIView!
     @IBOutlet weak var swatchView02: UIView!
--- 12.7 - Sprint 2 - Crayons/12.7 - Sprint 2 - Crayons/ViewController.swift
+++ 12.7 - Sprint 2 - Crayons/12.7 - Sprint 2 - Crayons/ViewController.swift
@@ -1,13 +1,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var crayon: Crayon?
+    
+    var heartEmoji: String = ""
+    var thinkEmoji: String = ""
+    var thinkOrHeart: String = ""
+    
+    override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+    }
+    
     @IBOutlet weak var swatchVIew01: UIView!
     @IBOutlet weak var swatchView02: UIView!
     @IBOutlet weak var swatchView03: UIView!
     @IBOutlet weak var swatchView04: UIView!
+    @IBOutlet weak var likeButton01: UIButton!
+    @IBOutlet weak var likeButton02: UIButton!
+    @IBOutlet weak var viewImage: UIImageView!
     
-
-
+    
+    @IBAction func likeAction01(_ sender: Any) {
+    }
+    
+    @IBAction func likeAction02(_ sender: Any) {
+    }
+    
 
 
 
