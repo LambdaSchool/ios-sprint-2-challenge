1707fc905765e7ab5fe585ab800ad612442d31bd
Vijay Das <vijaytdas@gmail.com>
2018-12-07 11:11:59 -0600

    add skeleton class files and setup the main view and detail view with all view objects and layout

---
+++ ColorList Challenge 2/ColorList Challenge 2/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  ColorList Challenge 2
+//
+//  Created by Vijay Das on 12/7/18.
+//  Copyright © 2018 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+}
---
+++ ColorList Challenge 2/ColorList Challenge 2/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  ColorList Challenge 2
+//
+//  Created by Vijay Das on 12/7/18.
+//  Copyright © 2018 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    static let reuseIdentifier = "cell"
+    
+    
+    
+}
