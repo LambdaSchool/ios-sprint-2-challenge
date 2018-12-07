598f9c59394df212fefcadf82a6648cb2a255eb9
Vijay Das <vijaytdas@gmail.com>
2018-12-07 15:37:54 -0600

    MVP

--- ColorList Challenge 2/ColorList Challenge 2/DetailViewController.swift
+++ ColorList Challenge 2/ColorList Challenge 2/DetailViewController.swift
@@ -9,46 +9,34 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-    
-    
     var crayon: Crayon?
     
     @IBOutlet weak var detailBorder: UIView!
     @IBOutlet weak var colorView: UIView!
     
-    
     @IBOutlet weak var crayonImage: UIImageView!
     @IBOutlet weak var swatchView: UIImageView!
- 
     
     @IBOutlet weak var colorName: UILabel!
-    
     
     @IBOutlet weak var likeTopButton: UIButton!
     @IBOutlet weak var likeBottonButton: UIButton!
  
-    
     @IBAction func topButtonPressed(_ sender: Any) {
-        crayon?.isLiked.toggle()
-//        updateViews()
+        updateViews()
     }
     
     @IBAction func bottonButtonPressed(_ sender: Any) {
-        crayon?.isLiked.toggle()
-//        updateViews()
+        updateViews()
     }
     
     override func viewDidLoad() {
-//        updateViews()
+        updateViews()
     }
-    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        
         guard let crayon = crayon else { return }
-        
         likeTopButton?.backgroundColor = crayon.color
         likeBottonButton?.backgroundColor = crayon.color
         detailBorder?.backgroundColor = crayon.color
@@ -56,41 +44,26 @@ class DetailViewController: UIViewController {
         colorName?.text = crayon.name
         crayonImage?.image = crayon.image
         swatchView?.backgroundColor = crayon.color
-//        likeTopButton.setTitle.("\(buttonEmoji)")
-        
-//        updateViews()
-        
+        updateViews()
     }
     
-//    var like: String = "❤️"
-//    var thinking: String = "🤔"
-//    var buttonEmoji: String = ""
-//
-//
-//    // Like button
-//
-//    // need a function that can be called in actions for both buttons and in view did load
-//    // that reflects state of like/not liked ... and updates the state of that crayon
-//    // isLiked variable
-//
-//    func updateViews() {
-//        guard let crayon = crayon else { return }
-//
-//
+    var buttonEmoji: String = ""
+    func toggleIsLiked(crayon: Crayon) {
+        crayon.isLiked = !crayon.isLiked
+        switch crayon.isLiked {
+        case true:
+            crayon.isLiked = true
+            buttonEmoji = "🤔"
+        case false:
+            crayon.isLiked = false
+            buttonEmoji = "❤️"
+       }
+  }
 
-      
-        
-        
-   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    func updateViews() {
+        guard let crayon = crayon else { return }
+        toggleIsLiked(crayon: crayon)
+        likeTopButton?.setTitle("\(buttonEmoji)", for: .normal)
+        likeBottonButton?.setTitle("\(buttonEmoji)", for: .normal)
+    }
 }
--- ColorList Challenge 2/ColorList Challenge 2/TableViewController.swift
+++ ColorList Challenge 2/ColorList Challenge 2/TableViewController.swift
@@ -13,11 +13,6 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
@@ -40,12 +35,11 @@ class TableViewController: UITableViewController {
         
     }
     
-        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return CrayonHelper.shared.rowCountFor(section: section)
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.reuseIdentifier, for: indexPath) as? TableViewCell else { fatalError("Could not dequeue cell")}
         
@@ -55,51 +49,12 @@ class TableViewController: UITableViewController {
         cell.nameLabel.text = crayon.name
         cell.crayonView.image = crayon.image
         cell.cellBorder.backgroundColor = .white
-        
-
-        // Configure the cell...
-
         return cell
     }
  
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 180
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     // MARK: - Navigation
 
@@ -115,8 +70,5 @@ class TableViewController: UITableViewController {
         
         destination.crayon = crayon
         
-        
-        
     }
-
 }
