d0d4dafc8ca69d668cedfe273e15d278b5884bf2
Vijay Das <vijaytdas@gmail.com>
2018-12-07 10:18:11 -0600

    add basic project and added assets and model filles

---
+++ ColorList Challenge 2/ColorList Challenge 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorList Challenge 2
+//
+//  Created by Vijay Das on 12/7/18.
+//  Copyright © 2018 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
