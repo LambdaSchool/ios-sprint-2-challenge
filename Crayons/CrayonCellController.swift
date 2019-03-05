24f544e39b94e819f47e870a61eb6b6a458e8d0f
Lotanna <sugabelly@gmail.com>
2019-03-05 01:31:39 -0500

    List view complete
    implemented custom cell for crayon cell, table view methods and used label, image view, and uiview to populate cell conditionally

---
+++ Crayons/CrayonCellController.swift
@@ -0,0 +1,17 @@
+//
+//  CrayonCellController.swift
+//  Crayons
+//
+//  Created by Lotanna Igwe-Odunze on 3/5/19.
+//  Copyright © 2019 Lotanna Igwe-Odunze. All rights reserved.
+//
+
+import UIKit
+
+class CrayonCellController: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var crayonView: UIImageView!
+    
+}
---
+++ Crayons/CrayonListController.swift
@@ -0,0 +1,39 @@
+//
+//  CrayonListController.swift
+//  Crayons
+//
+//  Created by Lotanna Igwe-Odunze on 3/5/19.
+//  Copyright © 2019 Lotanna Igwe-Odunze. All rights reserved.
+//
+
+import UIKit
+
+class CrayonListController: UITableViewController {
+    
+    let crayola = CrayonHelper.shared
+    
+    override func viewDidLoad() {
+        tableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return crayola.sectionCount
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return crayola.rowCountFor(section: section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "crynCell", for: indexPath) as! CrayonCellController
+        
+        let cellCrayon = crayola.crayonFor(indexPath: indexPath)
+        
+        cell.titleLabel.text = cellCrayon.name
+        cell.crayonView.image = cellCrayon.image
+        cell.contentView.backgroundColor = cellCrayon.color
+        
+        return cell
+    }
+}
+
